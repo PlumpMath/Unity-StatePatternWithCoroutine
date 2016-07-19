27cf66b67c7285c60ee86187488df35e472ac7b7
sekig <mkt.sekiguchi@gmail.com>
2016-07-19 14:23:19 +0900

    add extra methods

--- Assets/Scripts/IModeState.cs
+++ Assets/Scripts/IModeState.cs
@@ -2,9 +2,16 @@
 {
     public interface IModeState
     {
+        // common methods
+
         void EnterState(IMode context);
         void ExitState(IMode context);
         void GoNext(IMode context);
         void DumpName(IMode context);
+
+        // special methods
+
+        void SpecialA(IMode context);
+        void SpecialB(IMode context);
     }
 }
